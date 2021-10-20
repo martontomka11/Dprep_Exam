2f727d9d5dea8695a56e8d5624ffe6b2363ec4d8
martontomka11 <89907077+martontomka11@users.noreply.github.com>
2021-10-20 11:36:12 +0200

    Update pivot_table.R

--- src/pivot_table.R
+++ src/pivot_table.R
@@ -6,3 +6,5 @@ df_grouped <- read.csv("df_grouped.csv")
 
 # create pivot table
 df_pivot <- df_grouped %>% dcast(date ~ neighbourhood, fun.aggregate = sum, value.var = "num_reviews")
+
+write.csv(df_pivot, "data/df_pivot.csv")
